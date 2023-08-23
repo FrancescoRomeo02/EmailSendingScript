bdd216616f36a22d6f87eccd084ba13a297caf7e
Francesco <romeo.francesco.2002@gmail.com>
2023-08-23 23:51:54 +0200

    translation and added comments

--- mail.py
+++ mail.py
@@ -4,8 +4,7 @@ from getpass import getpass
 import smtplib
 from email.message import EmailMessage
 
-
-# ricerca file dati #
+# Search for data file #
 pre_decision = 0
 for root, dirs, files in os.walk(os.getcwd()):
     for file in files:
@@ -13,11 +12,10 @@ for root, dirs, files in os.walk(os.getcwd()):
             file_path = (os.path.join(root, file))
             pre_decision = 1
 
-# start
-dato = input('usare i dati predefiniti(s/n)? ')
-dato.upper().lower()
+# Start
+dato = input('Use predefined data (y/n)? ').strip().lower()  # Clean and convert to lowercase
 
-temp_date = []
+temp_data = []
 data = {
     "mail": "",
     "password": "",
@@ -25,28 +23,26 @@ data = {
     "path": ""
 }
 
-# controllo scelta
-if(dato == 's'and pre_decision != 0):
+# Check user's choice
+if dato == 'y' and pre_decision != 0:
     decision = 1
-elif(pre_decision == 0):
-    print('il file predefinito è stato cancellato.\n inserisci i dati manulamnete e lo creerò per te')
+elif pre_decision == 0:
+    print('The predefined file was deleted.\nEnter data manually and I will create it for you.')
     decision = 0
-elif(dato == 'n'):
+elif dato == 'n':
     decision = 0
 else:
     os.system("python mail.py")
 
-if(decision):
-    # standard #
-    d = open(file_path, "r")
+if decision:
+    # Use predefined data #
+    with open(file_path, "r") as d:
+        temp_data = [line.strip() for line in d]
 
-    for date_line in d:
-        temp_date.append(date_line)
-
-    data['mail'] = temp_date[0].rstrip("\n")
-    data['password'] = temp_date[1].rstrip("\n")
-    data["to"] = temp_date[2].rstrip("\n")
-    data['path'] = temp_date[3].rstrip("\n")
+    data['mail'] = temp_data[0]
+    data['password'] = temp_data[1]
+    data["to"] = temp_data[2]
+    data['path'] = temp_data[3]
 
     address = data['mail']
     password = data['password']
@@ -60,30 +56,30 @@ if(decision):
     msg.set_content('')
 
 else:
-    # modifica dati #
-    print('per utilizzare un profilo diverso da quello predefinito abilitare l\'accesso ad app meno sicure:\n https://myaccount.google.com/u/1/lesssecureapps \n\ninoltre se il profilo dispone della verifica in due passaggi usare come password quella per le app: \n https://myaccount.google.com/u/0/apppasswords?rapt = AEjHL4OHEKXhuBJJEiQLV-7kiULeXp3dME-JnZOp14JqJzohdhQOOlExWvg0eZ5ju5UjZr6VIZIOSDBloiGQgEVcu4f1WryX2w ')
+    # Enter new data #
+    print('To use a profile different from the default one, enable access to less secure apps:\nhttps://myaccount.google.com/u/1/lesssecureapps\n\nIf your profile has two-step verification, use the app password:\nhttps://myaccount.google.com/u/0/apppasswords?rapt=AEjHL4OHEKXhuBJJEiQLV-7kiULeXp3dME-JnZOp14JqJzohdhQOOlExWvg0eZ5ju5UjZr6VIZIOSDBloiGQgEVcu4f1WryX2w')
 
-    address = input('email mittente: ')
-    temp_date.append(address)
+    address = input('Sender email: ')
+    temp_data.append(address)
 
-    password = getpass('password mittente: ')
-    temp_date.append(password)
+    password = getpass('Sender password: ')
+    temp_data.append(password)
 
-    to = input('mail destinatario: ')
-    temp_date.append(to)
+    to = input('Recipient email: ')
+    temp_data.append(to)
 
-    path_file = input('percorso cartella file: ')
-    temp_date.append(path_file)
+    path_file = input('Folder path for files: ')
+    temp_data.append(path_file)
 
     with open("dati_predefiniti.txt", "w") as txt_file:
-        for line in temp_date:
-            txt_file.write(" ".join(line) + "\n")
+        for line in temp_data:
+            txt_file.write(line + "\n")
 
     msg = EmailMessage()
-    msg['Subject'] = input('oggetto: ')
+    msg['Subject'] = input('Subject: ')
     msg['From'] = address
     msg['To'] = to
-    msg.set_content(input('messaggio: '))
+    msg.set_content(input('Message: '))
 
 files = []
 for (dirpath, dirnames, filenames) in walk(path_file):
@@ -91,15 +87,14 @@ for (dirpath, dirnames, filenames) in walk(path_file):
     break
 
 for file in files:
-    with open(path_file+"/"+file, 'rb') as f:
+    with open(os.path.join(path_file, file), 'rb') as f:
         file_data = f.read()
         file_name = f.name
 
     msg.add_attachment(file_data, maintype='application',
                        subtype='octet-stream', filename=file_name)
 
-
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(address, password)
     smtp.send_message(msg)
-    input("email inviata con successo, premi invio per uscire")
+    input("Email sent successfully. Press Enter to exit.")
